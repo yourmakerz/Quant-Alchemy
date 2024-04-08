5043377356de4df9f434836e202a563311a6c9f2
Hassan Mehmood <mehassanhmood@gmail.com>
2024-04-08 01:18:20 -0400

    Page responsiveness

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ async def solutions(request:Request):
     return templates.TemplateResponse("solutions.html",{"request":request})
 
 @app.get("/research")
-async def solutions(request:Request):
+async def research(request:Request):
     return templates.TemplateResponse("research.html",{"request":request})
 
 @app.get("/investment")
-async def solutions(request:Request):
+async def investment(request:Request):
     return templates.TemplateResponse("investment.html",{"request":request})
