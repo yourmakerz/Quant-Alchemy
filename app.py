241b46527e65129d0895bbe4dd85dc6657b55c9d
Hassan Mehmood <mehassanhmood@gmail.com>
2024-04-12 00:23:07 -0400

    Bar chart --> Grouped Bar chart

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from fastapi.staticfiles import StaticFiles
 from scripts import relative_strength, quantitative_analysis
 import plotly.graph_objects as go
 import plotly.express as px
-from typing import Optional
+from typing import Union ,List, Optional
 
 
 
@@ -28,21 +28,34 @@ async def research(request:Request, symbol: Optional[str] = None):
     plot_html1 = fig1.to_html(full_html=False)
     fig = px.line(b,x=b.index,y=b.columns)
     plot_html2 = fig.to_html(full_html = False)
-
+    
     if symbol is None:
         financial_health_html = ""
         valuation_html = ""
-    else:
-        financial_health, valuation = quantitative_analysis.get_company_data(symbol=symbol)
+    else: 
+        if isinstance(symbol, str):
+            symbols = [symbol]
+        elif isinstance(symbol, list):
+            symbols = symbol
+        else:
+            raise ValueError("Symbol must be a string or a list of strings")
+        financial_health_html = ""
+        valuation_html = ""
+        symbols = [i for x in symbols for i in x.replace(" ","").split(",")]
+        print("------------------------------------------")
+        print(symbols)
+        
+       
+        financial_health, valuation = quantitative_analysis.get_company_data(symbol=symbols)
 
-        # Generate bar graphs for financial health and valuation using Plotly
+    # Generate bar graphs for financial health and valuation using Plotly
         financial_health_fig = px.bar(financial_health, x=financial_health.index, y=financial_health.columns,
-                                    labels={'x': 'Metrics', 'y': 'Values'}, title='Financial Health',log_y=True)
-        # valuation_fig = px.bar(valuation, x=valuation.columns[:-1], y=valuation.index,
-        #                         labels={'x': 'Metrics', 'y': 'Values'}, title='Valuation')
+                                labels={'x': 'Metrics', 'y': 'Values'}, title='Financial Health',log_y=True, barmode="group")
+    # valuation_fig = px.bar(valuation, x=valuation.columns[:-1], y=valuation.index,
+    #                         labels={'x': 'Metrics', 'y': 'Values'}, title='Valuation')
         valuation_fig = px.bar(valuation, x=valuation.index, y=valuation.columns,
-                        labels={'x': 'Metrics', 'y': 'Values'}, title='Valuation', log_y=True) 
-        # Convert the Plotly bar graphs to HTML code
+                    labels={'x': 'Metrics', 'y': 'Values'}, title='Valuation', log_y=True,  barmode="group") 
+    # Convert the Plotly bar graphs to HTML code
         financial_health_html = financial_health_fig.to_html(full_html=False)
         valuation_html = valuation_fig.to_html(full_html=False)
 
--- scripts/quantitative_analysis.py
+++ scripts/quantitative_analysis.py
@@ -21,53 +21,66 @@ def get_company_data(symbol, duration="quarter"):
             - The first DataFrame contains health-related financial metrics.
             - The second DataFrame contains valuation-related financial metrics.
     """
-    
+    if isinstance(symbol, str):
+            symbols = [symbol]
+    elif isinstance(symbol, list):
+            symbols = symbol
+    else:
+        raise ValueError("Symbol must be a string or a list of strings")
     # Retrieve company profile
-    company_profile = fmp.company_profile(apikey=fmp_key, symbol=symbol)
-    last = company_profile[0]['price']
-    mcap = company_profile[0]['mktCap']
-    industry = company_profile[0]['industry']
+
+    health_dataframes = []
+    valuation_dataframe = []
+    for symbol in symbols:
+        company_profile = fmp.company_profile(apikey=fmp_key, symbol=symbol)
+        last = company_profile[0]['price']
+        mcap = company_profile[0]['mktCap']
+        # industry = company_profile[0]['industry']
+        
+        # Retrieve key metrics
+        key_metrics = fmp.key_metrics_ttm(apikey=fmp_key, symbol=symbol)
+        eps_ttm = round(key_metrics[0]['netIncomePerShareTTM'], 2)
+        pe_ttm = round(key_metrics[0]['peRatioTTM'], 2)
+        
+        # Retrieve income statement
+        inc_statement = fmp.income_statement(apikey=fmp_key, symbol=symbol, period=duration)
+        shares = inc_statement[0]['weightedAverageShsOut']
+        net_income = inc_statement[0]['netIncome']
+        
+        # Retrieve balance sheet
+        financials = fmp.balance_sheet_statement(apikey=fmp_key, symbol=symbol, period=duration)
+        current_assets = financials[0]['cashAndShortTermInvestments'] + financials[0]['inventory'] + \
+                        (financials[0]['netReceivables'] / 2) + financials[0]['otherCurrentAssets']
+        cash_share = round(financials[0]['cashAndCashEquivalents'] / shares, 2)
+        total_ratio = round(financials[0]['totalAssets'] / financials[0]['totalLiabilities'], 2)
+        net_current_assets = current_assets - financials[0]['totalCurrentLiabilities']
+        liability_share = round(financials[0]['totalCurrentLiabilities'] / shares, 2)
+        tangible_assets = (financials[0]['propertyPlantEquipmentNet'] / 2) + net_current_assets
+        inv_yield = round(net_income / mcap, 3)
+        operating_margin = inc_statement[0]['operatingIncomeRatio']
+        bvps = round(tangible_assets / shares, 2)
+        bvps_price = bvps / last
+        current_ratio = round(financials[0]['totalCurrentAssets'] / financials[0]['totalCurrentLiabilities'], 2)
+        
+        # Retrieve earnings surprise
+        earnings = fmp.earnings_surprises(apikey=fmp_key, symbol=symbol)
+        earning = pd.DataFrame.from_dict(earnings)
+        earning.index = earning['date']
+        earning = earning.drop(columns='date')
+        earning = earning.sort_index()
+        fq_eps = earning.iloc[-5:, 1].mean()
+        
+        # Create data frames
+        company_health = {'BVPS': [bvps], 'Cash/share': [cash_share], 'liability_share': [liability_share],
+                        'EPS_ttm': [eps_ttm], 'fq_eps': [fq_eps],}
+        company_valuation = {'BVPS/Price': [bvps_price], 'P/E': [pe_ttm], 'Inv_Yield': [inv_yield],
+                            'Current_ratio': [current_ratio], 'Total_Ratio': [total_ratio], }
+        
+        health_df = pd.DataFrame(company_health, index=[symbol]).T
+        health_dataframes.append(health_df)
+        valuation_df = pd.DataFrame(company_valuation, index=[symbol]).T
+        valuation_dataframe.append(valuation_df)
+    health = pd.concat(health_dataframes)
+    valuation = pd.concat(valuation_dataframe)
     
-    # Retrieve key metrics
-    key_metrics = fmp.key_metrics_ttm(apikey=fmp_key, symbol=symbol)
-    eps_ttm = round(key_metrics[0]['netIncomePerShareTTM'], 2)
-    pe_ttm = round(key_metrics[0]['peRatioTTM'], 2)
-    
-    # Retrieve income statement
-    inc_statement = fmp.income_statement(apikey=fmp_key, symbol=symbol, period=duration)
-    shares = inc_statement[0]['weightedAverageShsOut']
-    net_income = inc_statement[0]['netIncome']
-    
-    # Retrieve balance sheet
-    financials = fmp.balance_sheet_statement(apikey=fmp_key, symbol=symbol, period=duration)
-    current_assets = financials[0]['cashAndShortTermInvestments'] + financials[0]['inventory'] + \
-                     (financials[0]['netReceivables'] / 2) + financials[0]['otherCurrentAssets']
-    cash_share = round(financials[0]['cashAndCashEquivalents'] / shares, 2)
-    total_ratio = round(financials[0]['totalAssets'] / financials[0]['totalLiabilities'], 2)
-    net_current_assets = current_assets - financials[0]['totalCurrentLiabilities']
-    liability_share = round(financials[0]['totalCurrentLiabilities'] / shares, 2)
-    tangible_assets = (financials[0]['propertyPlantEquipmentNet'] / 2) + net_current_assets
-    inv_yield = round(net_income / mcap, 3)
-    operating_margin = inc_statement[0]['operatingIncomeRatio']
-    bvps = round(tangible_assets / shares, 2)
-    bvps_price = bvps / last
-    current_ratio = round(financials[0]['totalCurrentAssets'] / financials[0]['totalCurrentLiabilities'], 2)
-    
-    # Retrieve earnings surprise
-    earnings = fmp.earnings_surprises(apikey=fmp_key, symbol=symbol)
-    earning = pd.DataFrame.from_dict(earnings)
-    earning.index = earning['date']
-    earning = earning.drop(columns='date')
-    earning = earning.sort_index()
-    fq_eps = earning.iloc[-5:, 1].mean()
-    
-    # Create data frames
-    company_health = {'BVPS': [bvps], 'Cash/share': [cash_share], 'liability_share': [liability_share],
-                      'EPS_ttm': [eps_ttm], 'fq_eps': [fq_eps],}
-    company_valuation = {'BVPS/Price': [bvps_price], 'P/E': [pe_ttm], 'Inv_Yield': [inv_yield],
-                         'Current_ratio': [current_ratio], 'Total_Ratio': [total_ratio], }
-    
-    health_df = pd.DataFrame(company_health, index=[symbol])
-    valuation_df = pd.DataFrame(company_valuation, index=[symbol])
-    
-    return health_df.T, valuation_df.T
+    return health, valuation
